44d9ccd69997d6f1031e1392feb1d83919a557bd
kellyrobs <kelly.evangelista@cscdoesporte.com.br>
2018-11-07 14:48:16 -0200

    Iniciando o etudo para o sistema de disparo de newsletter ambassadors futebol

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR" prefix="og: http://ogp.me/ns#">
+<head>
+
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	
+	<meta name="theme-color" content="#173546">
+	<meta name="author" content="Kelly Roberta - Dev_Robs">
+	<meta name="description" content="Sistema de Newsletter Missão Ambassadors">
+	<meta name="keywords" content="futebol,">
+
+	<title>Newsletter Ambassadors</title>
+    </head>
+</html>
